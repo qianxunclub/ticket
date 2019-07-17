160ca1dc78c76b2d21b2b139acb25011dce8eed1
张斌 <960339491@qq.com>
2019-07-17 14:49:33 +0800

    添加购票接口

--- src/main/java/com/qianxunclub/ticket/controller/Index.java
+++ src/main/java/com/qianxunclub/ticket/controller/Index.java
@@ -1,11 +1,16 @@
 package com.qianxunclub.ticket.controller;
 
+import com.qianxunclub.ticket.model.MyTicketInfoModel;
+import com.qianxunclub.ticket.thread.Start;
 import com.qianxunclub.ticket.thread.UserInfo;
 
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import lombok.AllArgsConstructor;
 
 /**
  * @author zhangbin
@@ -13,13 +18,22 @@ import org.springframework.web.bind.annotation.RestController;
  * @description: TODO
  */
 @RestController
+@AllArgsConstructor
 @RequestMapping(value = "/api", produces = "application/json")
 public class Index {
 
+    private Start start;
+
     @ResponseBody
-    @RequestMapping(value = "user/buying",method = RequestMethod.GET)
-    public Object user(){
+    @RequestMapping(value = "user",method = RequestMethod.GET)
+    public Object buying(){
         return UserInfo.myTicketInfoModelList;
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "user",method = RequestMethod.POST)
+    public void user(@RequestBody MyTicketInfoModel myTicketInfoModel){
+        start.add(myTicketInfoModel);
     }
 
 }
