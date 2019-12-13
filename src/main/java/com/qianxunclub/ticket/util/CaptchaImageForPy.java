4fa5ed4800e39b819983b9a3f91ad3a1b4c79659
张斌 <960339491@qq.com>
2019-12-13 16:45:16 +0800

    验证码编码问题

--- src/main/java/com/qianxunclub/ticket/util/CaptchaImageForPy.java
+++ src/main/java/com/qianxunclub/ticket/util/CaptchaImageForPy.java
@@ -50,7 +50,7 @@ public class CaptchaImageForPy {
             String os = System.getProperty("os.name");
             Process process;
             if (os.toLowerCase().startsWith("win")) {
-                String[] cmd = new String[]{"cmd", "/c", " cd python  &  set PYTHONIOENCODING=utf-8 & python main.py " + "..\\temp\\" + filename};
+                String[] cmd = new String[]{"cmd", "/c", " cd python  &  set PYTHONIOENCODING=" + System.getProperty("file.encoding") + " & python main.py " + "..\\temp\\" + filename};
                 process = runtime.exec(cmd);
             } else {
                 String bash = config.getPythonPath() + "/run.sh ../temp/" + filename;
@@ -75,7 +75,7 @@ public class CaptchaImageForPy {
     }
 
     public String get(InputStream inputStream) throws IOException {
-        InputStreamReader inputStreamReader = new InputStreamReader(inputStream, "utf-8");
+        InputStreamReader inputStreamReader = new InputStreamReader(inputStream, System.getProperty("file.encoding"));
         BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
         String line;
         PredictVO predictVO = new PredictVO();
